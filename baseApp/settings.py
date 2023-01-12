02cacdc59cab825ead9baae6aceeba8c651088a6
mohamed-mehdi-hyad <mehdihyad.mh@gmail.com>
2023-01-12 21:39:45 +0100

    trying deploying to railway

--- baseApp/settings.py
+++ baseApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5%*o2dltaelsu&b^-$#i19g2!$ub3-hnr6run(xhy12p@4^a66
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
