4a3f60585675388f94d0e574667794dc6d4dc372
mohamed-mehdi-hyad <mehdihyad.mh@gmail.com>
2023-01-13 15:02:37 +0100

    adding csrf_trusted_origin

--- baseApp/settings.py
+++ baseApp/settings.py
@@ -123,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://web-production-87d2.up.railway.app/']
